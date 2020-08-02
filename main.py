c45824978ed108e597f3e1fedafa4b2159772919
John Salas <johndsalas@gmail.com>
2020-08-01 23:29:25 -0500

    full game loop is running

--- main.py
+++ main.py
@@ -7,31 +7,31 @@ import hud as h
 import generate as g
 import spend as s
 import event as e
-import end as end
+import end as ed
 import random
 
 # Beginning Values
 fuel = 30
 food = 50
-power = 5
+power = 10
 hull = 5
 crew = 10
 burn_rate = 10
 morale = 100
 distance = 20
 day = 10
-    
+
 # Display Menu
 running = m.menu()
 
 # begin game loop
 while running == True:
 
-    # introduction 
+    # introduction
     if day == 10:
-       
+
         fuel, food, power, hull, crew, morale = i.intro(fuel, food, power, hull, crew, morale) 
-        
+
     # Display HUD
     h.hud(fuel, food, power, hull, crew, morale, distance, day)
 
@@ -40,22 +40,20 @@ while running == True:
 
     # spend resources
     fuel, food, power, hull, crew, burn_rate, morale, distance = s.spend(fuel, food, power, hull, crew, burn_rate, morale, distance)
-    
+
     # Check for loss conditions
-    running = end.loss(hull, crew, power, morale)
+    running = ed.loss(hull, crew, power, morale)
 
     # Event
     fuel, food, power, hull, crew, burn_rate, morale, distance = e.event(fuel, food, power, hull, crew, burn_rate, morale, distance)
 
     # Check for loss conditions
-    running = end.loss(hull, crew, power, morale)
+    running = ed.loss(hull, crew, power, morale)
 
     # Check for win conditions
-    running = end.win(distance, running)
+    running = ed.win(distance, running)
 
     # Update Day
     day -= 1
 
 print("You have exited the game. Thank you for playing!")
-
-
