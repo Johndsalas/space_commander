fb8d8d77703243161e32508288a2b12ceca15723
John Salas <johndsalas@gmail.com>
2020-08-16 22:20:25 -0500

    added play again loop to main

--- main.py
+++ main.py
@@ -11,56 +11,88 @@ import travel as t
 import end as ed
 import random
 
-# Beginning Values
-fuel = 30
-food = 50
-power = 10
-hull = 5
-crew = 10
-burn_rate = 10
-morale = 100
-distance = 20
-day = 10
+
+def game_loop():
+
+    # Beginning Values
+    fuel = 30
+    food = 50
+    power = 10
+    hull = 5
+    crew = 10
+    burn_rate = 10
+    morale = 100
+    distance = 20
+    day = 10
+
+    # begin game loop
+    running = True
+    while running:
+
+        # introduction
+        if day == 10:
+
+            fuel, food, power, hull, crew, morale = i.intro(fuel, food, power, hull, crew, morale) 
+
+        # Display Distance
+        h.status(distance, day)
+
+        # Phase 1: Production Phase
+        fuel, food, power, hull, crew, morale = p.production_phase(fuel, food, power, hull, crew, morale)
+
+        # Phase 2: Spend Phase
+        fuel, food, power, hull, crew, burn_rate, morale = s.spend(fuel, food, power, hull, crew, burn_rate, morale, distance)
+
+        # Check for loss conditions
+        if ed.lose(hull, crew, power, morale):
+            break
+
+        # Phase 3: Event Phase
+        fuel, food, power, hull, crew, morale, distance = e.event(fuel, food, power, hull, crew, morale, distance)
+
+        # Check for loss conditions
+        if ed.lose(hull, crew, power, morale):
+            break
+
+        # phase 4: Travel Phase
+        distance = t.travel(distance, distance_traveled)
+
+        # Check for win conditions
+        if ed.win(distance, running):
+            break
+
+        # Update Day
+        day -= 1
+
+    replay = True
+    while replay:
+    
+        print("Would you like to play again? y/n")
+        print("")
+        again = input()
+
+        if again == "y":
+
+            game_loop()
+
+        elif again == "n":
+
+            break
+
+        else:
+
+            print("invalid input. Please enter y or n.")
+
+
+
 
 # Display Menu
-running = m.menu()
+m.menu()
 
-# begin game loop
-while running == True:
+game_loop()
 
-    # introduction
-    if day == 10:
 
-        fuel, food, power, hull, crew, morale = i.intro(fuel, food, power, hull, crew, morale) 
 
-    # Display Distance
-    h.status(distance, day)
 
-    # Phase 1: Production Phase
-    fuel, food, power, hull, crew, morale = p.production_phase(fuel, food, power, hull, crew, morale)
-
-    # Phase 2: Spend Phase
-    fuel, food, power, hull, crew, burn_rate, morale = s.spend(fuel, food, power, hull, crew, burn_rate, morale, distance)
-
-    # Check for loss conditions
-    if ed.lose(hull, crew, power, morale):
-        break
-
-    # Phase 3: Event Phase
-    fuel, food, power, hull, crew, morale, distance = e.event(fuel, food, power, hull, crew, morale, distance)
-
-    # Check for loss conditions
-    if ed.lose(hull, crew, power, morale):
-        break
-
-    # phase 4: Travel Phase
-    distance = t.travel(distance, distance_traveled)
-
-    # Check for win conditions
-    if ed.win(distance, running):
-        break
-
-    # Update Day
-    day -= 1
 
 print("You have exited the game. Thank you for playing!")
