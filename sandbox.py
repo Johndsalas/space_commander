25f7cd14665b5750125689cd6f6eec50721534a8
John Salas <johndsalas@gmail.com>
2020-11-20 19:42:10 -0600

    rewrite of the story so far..

--- sandbox.py
+++ sandbox.py
@@ -1,13 +1,8 @@
-import random as r
-
-
-li = [1,2,3,4,5]
-
-for number in li:
-
-    print(number)
-
-    if number == 3:
-
-        break
-
+  As you arrive within scanning distance of Star Gazer 1, you find only broken metal and empty space
+    where a space station should be. To make matters worse, sensors are picking up several Garquackien
+    ships still in the area. Thanks to your years, in the captain’s seat, you quickly piece together
+    what has happened. The Garquackiens must have amassed a large armada, the likes of which the 
+    universe had never seen, in an attempt to destroy Earth Force and take over the galaxy. It is 
+    likely that they attacked here in mass to prevent their real target from readying its defenses, 
+    and their real target is Earth! You predict that it will take them 10 Intergalactic Standard Weeks 
+    to reach Earth. You must reach earth before they do, and sound the alarm. If you don’t Earth is
