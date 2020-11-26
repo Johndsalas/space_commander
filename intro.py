644225f53eb32c5ee8f3aaced0852e68417f7a63
John Salas <johndsalas@gmail.com>
2020-11-25 23:46:02 -0600

    intro complete

--- intro.py
+++ intro.py
@@ -1,5 +1,6 @@
 import  random
 
+
 def intro(fuel, food, power, hull, crew, morale):
 
     lost_fuel = random.randint(1,10)
@@ -9,31 +10,36 @@ def intro(fuel, food, power, hull, crew, morale):
     lost_crew = random.randint(0,3)
     lost_morale = random.randint(20,40)
 
-    # fuel -= lost_fuel
-    # food -= lost_food
-    # power -= lost_power
-    # hull -= lost_hull
-    # crew -= lost_crew
-    # morale -= lost_morale  
-    
+    fuel -= lost_fuel
+    food -= lost_food
+    power -= lost_power
+    hull -= lost_hull
+    crew -= lost_crew
+    morale -= lost_morale  
 
-    print("You narrowly escape the clutches of the Garquackien kill squad, and have managed avoid ")
-    print("detection for the moment. During this respite, your mind turns to the status of your ship and its ")
-    print("crew. Your last mission had been successful (for what that was worth) but also taxing. Your ")
-    print("resources, crew and even the ship itself, have been under considerable strain already. You can ")
-    print("only hope that the ship will hold together until you make it back to earth. With a heavy heart")
-    print("you raise your eyes to the HUD and examine what remains of your resources.")
+    print("The Story Continues…")
+    print("")
+    print("You manage to elude or destroy all of the attacking ships, but not without sustaining damage of")
+    print("your own. Between this latest attack and completing the last mission your supplies are")
+    print("beginning to dwindle. The ship is self-sustaining to a point, if its resources are properly")
+    print("managed. You can only hope that the ship will hold together long enough to make it back to")
+    print("Earth and raise the alarm. Feeling the weight of your new responsibility, you raise your eyes to ")
+    print("the HUD and examine what remains of your resources and crew...")
     print("")
     print("press ENTER to continue")
     input()
     print("")
 
-    # print("After escaping the Garquackien ships you recive rhe following status report")
-    # print(f"Lost crew members: {lost_crew}")
-    # print(f"Damaged fuel reserves: {lost_fuel} units")
-    # print(f"Damaged food reserves: {lost_food} units")
-    # print(f"Damaged power crystals: {lost_power}")
-    # print(f"Damage to hull: {lost_hull}")
-    # print(f"The crew's morale is holding at {morale}%")
+    print("After escaping the Garquackien ships you recive rhe following status report:")
+    print("")
+    print(f"Lost crew members: {lost_crew}")
+    print("")
+    print(f"Missing fuel reserves: {lost_fuel}")
+    print(f"Missing food reserves: {lost_food}")
+    print(f"Missing power crystals: {lost_power}")
+    print("")
+    print(f"Damage to hull: {lost_hull}")
+    print("")
+    print(f"The crew's morale is at {morale}%")
 
     return fuel, food, power, hull, crew, morale
--- sandbox.py
+++ sandbox.py
@@ -1,8 +1,6 @@
-  As you arrive within scanning distance of Star Gazer 1, you find only broken metal and empty space
-    where a space station should be. To make matters worse, sensors are picking up several Garquackien
-    ships still in the area. Thanks to your years, in the captain’s seat, you quickly piece together
-    what has happened. The Garquackiens must have amassed a large armada, the likes of which the 
-    universe had never seen, in an attempt to destroy Earth Force and take over the galaxy. It is 
-    likely that they attacked here in mass to prevent their real target from readying its defenses, 
-    and their real target is Earth! You predict that it will take them 10 Intergalactic Standard Weeks 
-    to reach Earth. You must reach earth before they do, and sound the alarm. If you don’t Earth is
+You narrowly escape the clutches of the Garquackien kill squad, and have managed avoid
+detection for the moment. During this respite, your mind turns to the status of your ship and its
+crew. Your last mission had been successful (for what that was worth) but also taxing. Your
+resources, crew and even the ship itself, have been under considerable strain already.You can
+only hope that the ship will hold together until you make it back to earth. With a heavy heart
+you raise your eyes to the HUD and examine what remains of your resources.
